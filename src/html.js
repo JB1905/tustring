ff781237b36599241bd85f5d45ef1993ea89ef1b
Jakub Biesiada <biesiadajakub@icloud.com>
2020-06-04 13:38:17 +0200

    Update

---
+++ src/html.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+const HTML = props => (
+  <html {...props.htmlAttributes}>
+    <head>
+      <meta charSet="utf-8" />
+      <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta
+        name="apple-mobile-web-app-status-bar-style"
+        content="black-translucent"
+      />
+      <meta
+        name="viewport"
+        content="viewport-fit=cover, width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0"
+      />
+
+      {props.headComponents}
+    </head>
+
+    <body {...props.bodyAttributes}>
+      {props.preBodyComponents}
+
+      <noscript key="noscript" id="gatsby-noscript">
+        This app works best with JavaScript enabled.
+      </noscript>
+
+      <div
+        key="body"
+        id="___gatsby"
+        dangerouslySetInnerHTML={{ __html: props.body }}
+      />
+
+      {props.postBodyComponents}
+    </body>
+  </html>
+)
+
+export default HTML

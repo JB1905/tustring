ba6b51f37d12b23ad6b81ee723c1110bb034a08e
Jakub Biesiada <biesiadajakub@icloud.com>
2020-06-11 14:44:37 +0200

    Update splash screen

--- src/html.js
+++ src/html.js
@@ -5,7 +5,9 @@ const HTML = props => (
     <head>
       <meta charSet="utf-8" />
       <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-capable" content="yes">
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="apple-touch-fullscreen" content="yes" />
       <meta
         name="apple-mobile-web-app-status-bar-style"
         content="black-translucent"

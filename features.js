a0cb8217bdf47bef50a621ff94f292da9fab1929
Jakub Biesiada <biesiadajakub@icloud.com>
2020-08-03 18:05:47 +0200

    Enable feature

--- features.js
+++ features.js
@@ -2,7 +2,7 @@ const featureFlags = {
   footerOpenSource: false,
   comments: false,
   tags: false,
-  searchForm: false,
+  searchForm: true,
   filters: false,
   fixedPWAiOS: false,
   sources: false,

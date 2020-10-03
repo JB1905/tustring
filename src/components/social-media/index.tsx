71e69dcdb4ab4771d57cb71d30b6bb645f864ea1
Jakub Biesiada <biesiadajakub@icloud.com>
2020-10-03 20:35:32 +0200

    chore: update

--- src/components/social-media/index.tsx
+++ src/components/social-media/index.tsx
@@ -3,19 +3,17 @@ import { SocialMediaWrapper, SocialMediaIcon } from './social-media.styles'
 import type { SocialMediaItem } from '../../types/SocialMediaItem'
 
 interface Props {
-  items: SocialMediaItem[]
+  readonly items: SocialMediaItem[]
 }
 
-const SocialMedia = ({ items }: Props) => {
-  return (
-    <SocialMediaWrapper>
-      {items.map(({ href, name, icon }) => (
-        <a href={href} aria-label={name} key={name}>
-          <SocialMediaIcon icon={icon} />
-        </a>
-      ))}
-    </SocialMediaWrapper>
-  )
-}
+const SocialMedia = ({ items }: Props) => (
+  <SocialMediaWrapper>
+    {items.map(({ href, name, icon }) => (
+      <a href={href} aria-label={name} key={name}>
+        <SocialMediaIcon icon={icon} />
+      </a>
+    ))}
+  </SocialMediaWrapper>
+)
 
 export default SocialMedia

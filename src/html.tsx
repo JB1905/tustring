154846675539391f226c1e49baa2daa02e9b93a8
Jakub Biesiada <biesiadajakub@icloud.com>
2021-01-01 22:10:25 +0100

    chore: bump packages

--- src/html.tsx
+++ src/html.tsx
@@ -298,7 +298,7 @@ const HTML = (props: Props) => {
         {props.preBodyComponents}
 
         <noscript key="noscript" id="gatsby-noscript">
-          This app works best with JavaScript enabled.
+          Ta aplikacja działa najlepiej z włączoną obsługą JavaScript.
         </noscript>
 
         <div
